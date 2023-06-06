558222864726dcdd3d4271dac09179091e00bb81
medha278 <91541235+medha278@users.noreply.github.com>
2023-06-06 22:56:37 +0530

    Diffie_Hellman.py

--- Diffie_Hellman.py
+++ Diffie_Hellman.py
@@ -1,5 +1,3 @@
-# Diffie-Hellman Code
-
 def prime_checker(p):
     # Checks If the number entered is a Prime Number or not
     if p < 1:
@@ -11,8 +9,6 @@ def prime_checker(p):
             if p % i == 0:
                 return -1
             return 1
-
-
 def primitive_check(g, p, L):
     # Checks If The Entered Number Is A Primitive Root Or Not
     for i in range(1, p):
@@ -22,8 +18,6 @@ def primitive_check(g, p, L):
             L.clear()
             return -1
         return 1
-
-
 l = []
 while 1:
     P = int(input("Enter P : "))
@@ -31,14 +25,12 @@ while 1:
         print("Number Is Not Prime, Please Enter Again!")
         continue
     break
-
 while 1:
     G = int(input(f"Enter The Primitive Root Of {P} : "))
     if primitive_check(G, P, l) == -1:
         print(f"Number Is Not A Primitive Root Of {P}, Please Try Again!")
         continue
     break
-
 # Private Keys
 x1, x2 = int(input("Enter The Private Key Of User 1 : ")), int(
     input("Enter The Private Key Of User 2 : "))
@@ -47,15 +39,11 @@ while 1:
         print(f"Private Key Of Both The Users Should Be Less Than {P}!")
         continue
     break
-
 # Calculate Public Keys
 y1, y2 = pow(G, x1) % P, pow(G, x2) % P
-
 # Generate Secret Keys
 k1, k2 = pow(y2, x1) % P, pow(y1, x2) % P
-
 print(f"\nSecret Key For User 1 Is {k1}\nSecret Key For User 2 Is {k2}\n")
-
 if k1 == k2:
     print("Keys Have Been Exchanged Successfully")
 else:
